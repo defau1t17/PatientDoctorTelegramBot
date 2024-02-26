66d675086a11e8db70e0a487e4ae5ade1deb8d6b
moto_default_motive <mihail.stepushin@gmail.com>
2024-02-27 00:07:12 +0300

    Doctor Telegram Bot Module

---
+++ DoctorTelegramBot/src/main/java/org/telegrambots/doctortelegrambot/DoctorTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package org.telegrambots.doctortelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorTelegramBotApplication.class, args);
+    }
+
+}
---
+++ DoctorTelegramBot/src/test/java/org/telegrambots/doctortelegrambot/DoctorTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.telegrambots.doctortelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
